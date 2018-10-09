3950f27dd69fe37a65ea2fda6436558c271d37f1
ZigZagNinja <36819963+ZigZagNinja@users.noreply.github.com>
2018-10-09 13:13:42 +0200

    Update javascript.js

--- Name/actualDart/javascript.js
+++ Name/actualDart/javascript.js
@@ -47,7 +47,7 @@ ctx.fill();
 //choose between 501 and 301
 let threeoneButton = new CanvasSubmit(canvas, {
 	x: 0, 
-	y: window.innerHeight,
+	y: window.innerHeight-100,
 	width: 100,
 	height: 100,
 	placeholder: '301',
@@ -58,8 +58,8 @@ let threeoneButton = new CanvasSubmit(canvas, {
 })
 
 let fiveoneButton = new CanvasSubmit(canvas, {
-	x: window.innerWidth, 
-	y: window.innerHeight,
+	x: window.innerWidth-100, 
+	y: window.innerHeight-100,
 	width: 100,
 	height: 100,
 	placeholder: '501',
