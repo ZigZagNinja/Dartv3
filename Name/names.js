0451fad37983eba62b156b575cf519c907d5549c
ZigZagNinja <36819963+ZigZagNinja@users.noreply.github.com>
2018-10-09 09:07:14 +0200

    Update names.js

--- Name/names.js
+++ Name/names.js
@@ -5,14 +5,8 @@ function buttonClicked(){
     name1 = document.getElementById("playerone").value;
     name2 = document.getElementById("playertwo").value;
 
-    window.open("file:///C:/Users/Mark/Desktop/some%20Tests%20v2/Dart/v2/Name/actualDart/index.html","_self");
+    window.open("https://zigzagninja.github.io/Dartv2/actualDart/index.html","_self");
 }
-
-
-#include <AutoItConstants.au3>
-#include <Date.au3>
-#include <Math.au3>
-#include <MsgBoxConstants.au3>
 
 
 
