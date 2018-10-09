4f7bef4aa728f61e5cd0e2d741423daf09248f3c
ZigZagNinja <36819963+ZigZagNinja@users.noreply.github.com>
2018-10-09 09:28:04 +0200

    Update names.js

--- Name/names.js
+++ Name/names.js
@@ -5,7 +5,7 @@ function buttonClicked(){
     name1 = document.getElementById("playerone").value;
     name2 = document.getElementById("playertwo").value;
 
-    window.open("https://zigzagninja.github.io/Dartv2/actualDart/index.html","_self");
+    window.open("https://zigzagninja.github.io/Dartv3/Names/actualDart/index.html","_self");
 }
 
 
